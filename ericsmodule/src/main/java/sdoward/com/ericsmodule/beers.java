7e45a6110da56d57826d94b36f300bd6393b89cc
Eric <erickowa93@gmail.com>
2018-11-02 05:31:28 +0300

    Update ericsmodule/src/main/java/sdoward/com/ericsmodule/beers.java
    Add task 3

---
+++ ericsmodule/src/main/java/sdoward/com/ericsmodule/beers.java
@@ -0,0 +1,18 @@
+package sdoward.com.ericsmodule;
+
+public class beers {
+
+    int id;
+    String name;
+    String tagline;
+    String first_brewed;
+    String description;
+    String image_url;
+    Float abv;
+    int attenuation_level;
+    String brewers_tips;
+    String contributed_by;
+
+
+
+}
