bb1e48dde1002f4a63a9921a44a4288ef0c738f8
mrBucks <riqomach@gmail.com>
2019-02-21 20:16:53 +0300

    onClick Listener

--- ericsmodule/src/main/java/sdoward/com/ericsmodule/BeerAdapter.java
+++ ericsmodule/src/main/java/sdoward/com/ericsmodule/BeerAdapter.java
@@ -1,12 +1,16 @@
 package sdoward.com.ericsmodule;
 
 import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import retrofit2.http.Url;
@@ -45,6 +49,14 @@ public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
                 .load(beer.getImageUrl())
                 .into(beerViewHolder.imageView);
 
+        beerViewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(context, GalleryActivity.class);
+                intent.putExtra("image_url", Image.get());
+            }
+        });
+
         //beerViewHolder.imageView.setImageBitmap(beer.image_url);
         beerViewHolder.IdTextView.setText(String.valueOf(beer.getId()));
         beerViewHolder.nameTextView.setText(beer.getName());
@@ -71,6 +83,7 @@ public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
         public TextView abvTextView;
         public TextView attenuationLevelTextView;
         public TextView descriptionTextView;
+        public RelativeLayout parentLayout;
 
 
         public ViewHolder(@NonNull View itemView) {
@@ -83,6 +96,7 @@ public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
             this.abvTextView=itemView.findViewById(R.id.abvTextView);
             this.attenuationLevelTextView=itemView.findViewById(R.id.attenuationLevelTextView);
             this.descriptionTextView=itemView.findViewById(R.id.descriptionTextView);
+            this.parentLayout=itemView.findViewById(R.id.parent_layout);
         }
 
 
